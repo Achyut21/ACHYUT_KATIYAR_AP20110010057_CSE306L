7068f3823eafb692c04a0f035a2c61f4842a547a
Achyut katiyar <88200447+Achyut21@users.noreply.github.com>
2022-09-29 12:13:38 +0530

    Update RDP2.c

--- WEEK - 6/RDP2.c
+++ WEEK - 6/RDP2.c
@@ -1,129 +1,96 @@
-[11:47, 9/29/2022] Attin katiyar: #include<stdio.h>
+//S ---> (L) | a
+
+//L ---> SL`
+
+//L` ---> ,SL` | e
+
+#include<stdio.h>
 #include<string.h>
-int E(),Edash(),T(),Tdash(),F();
+int S(),L(),Ldash();
 char *ip;
 char string[50];
 int main()
 {
-printf("Enter the string\n");
-scanf("%s",string);
-ip=string;
-printf("\n\nInput\tAction\n--------------------------------\n");
-
-if(E() && ip==’\0’){
-printf("\n--------------------------------\n");
-printf("\n String is successfully parsed\n");
+    printf("Enter the string\n");
+    scanf("%s",string);
+    ip=string;
+    printf("\n\nInput\tAction\n--------------------------------\n");
+    
+    if(L() && *ip=='\0')
+    {
+        printf("\n--------------------------------\n");
+        printf("\n String is successfully parsed\n");
+    }
+    else
+    {
+        printf("\n--------------------------------\n");
+        printf("Error in parsing String\n");
+    }
 }
-else{
-printf("\n--------------------------------\n");
-printf("Error in parsing String\n");
-}
-}
-int E()
+int S()
 {
-printf("%s\tE->TE' \n",ip);
-if(T())
-{
-if(Edash())
-{
-return 1;
+    if(*ip=='(')
+    {
+       printf("%s\tS->(L) \n",ip);
+        ip++;
+        if(L())
+        {
+            if(*ip==')')
+            {
+                ip++;
+                return 1;
+            }
+            else
+                return 0;
+        }
+        else
+            return 0;
+    }
+    else if(*ip=='a')
+    {
+        ip++;
+        printf("%s\tS->a \n",ip);
+        return 1;
+    }
+    else
+        return 0;
 }
-else
-return 0;
-}
-else
-return 0;
-}
-int Edash()
+int L()
 {
-if(*ip=='+')
-{
-printf("%s\tE'->+TE' \n",ip);
-ip++;
-if(T())
-{
-if(Edash())
-{
-return 1;
+    printf("%s\tS->SL' \n",ip);
+    if(S())
+    {
+        if(Ldash())
+        {
+            return 1;
+        }
+        else
+            return 0;
+    }
+    else
+        return 0;
 }
-else
-return 0;
-}
-else
-return 0;
-}
-else
+int Ldash()
 {
-printf("%s\tE'->^ \n",ip);
-return 1;
-}
-[11:47, 9/29/2022] Attin katiyar: }
-int T()
-{
-printf("%s\tT->FT' \n",ip);
-if(F())
-{
-
-if(Tdash())
-{
-return 1;
-}
-else
-return 0;
-}
-else
-return 0;
-}
-int Tdash()
-{
-if(ip=='')
-{
-printf("%s\tT'->*FT' \n",ip);
-ip++;
-if(F())
-{
-if(Tdash())
-{
-return 1;
-}
-else
-return 0;
-}
-else
-return 0;
-}
-else
-{
-printf("%s\tT'->^ \n",ip);
-return 1;
-}
-}
-int F()
-{
-if(*ip=='(')
-{
-printf("%s\tF->(E) \n",ip);
-ip++;
-if(E())
-{
-if(*ip==')')
-{
-ip++;
-return 0;
-}
-else
-return 0;
-}
-else
-return 0;
-}
-
-else if(*ip=='i')
-{
-ip++;
-printf("%s\tF->id \n",ip);
-return 1;
-}
-else
-return 0;
+    if(*ip==',')
+    {
+        printf("%s\tL'->,SL' \n",ip);
+        ip++;
+        if(S())
+        {
+            if(Ldash())
+            {
+                return 1;
+            }
+            else
+                return 0;
+        }
+        else
+            return 0;
+    }
+    else
+    {
+        printf("%s\tL'->^ \n",ip);
+        return 1;
+    }
 }
